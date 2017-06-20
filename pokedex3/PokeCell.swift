ade863b19f8cb4168d69ba642a2db56ca6b8d9c6
PJ Desamero <pjdesi08@gmail.com>
2017-06-20 15:48:10 -0500

    Added PokeCell class and connected it to storyboard

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Paul Desamero on 6/20/17.
+//  Copyright © 2017 PJDesi. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+    
+}
