0b356cf08d6535ec74e88db19c38f483342896dc
PJ Desamero <pjdesi08@gmail.com>
2017-06-20 16:04:13 -0500

    Added collection view delegate, data source, flow layout

--- pokedex3/ViewController.swift
+++ pokedex3/ViewController.swift
@@ -10,12 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+        collection.dataSource = self
+        collection.delegate = self
         
     }
 
 }
 
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row)
+            cell.configureCell(pokemon: pokemon)
+            
+            return cell
+        
+        } else {
+            
+            return UICollectionViewCell()
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
+    }
+    
+}
+
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+}
