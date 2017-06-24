ec6a71c8c56dccf34981de9cb0969d18dcb2b1cd
Paul Desamero <paul.j.desamero@ehi.com>
2017-06-24 17:34:52 -0500

    Added functionality to searchbar

--- pokedex3/ViewController.swift
+++ pokedex3/ViewController.swift
@@ -13,16 +13,22 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var pokemon = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
     var musicPlayer: AVAudioPlayer!
     
+    var inSearchMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collection.dataSource = self
         collection.delegate = self
+        
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
         
         parsePokemonCSV()
         initAudio()
@@ -34,7 +40,6 @@ class ViewController: UIViewController {
         let path = Bundle.main.path(forResource: "music", ofType: "mp3")
         
         do {
-            
             musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
             musicPlayer.prepareToPlay()
             musicPlayer.numberOfLoops = -1
@@ -46,6 +51,7 @@ class ViewController: UIViewController {
         
     }
     
+    
     func parsePokemonCSV() {
         
         let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
@@ -55,7 +61,7 @@ class ViewController: UIViewController {
             let rows = csv.rows
             
             for row in rows {
-                
+            
                 let pokeId = Int(row["id"]!)!
                 let name = row["identifier"]!
                 
@@ -68,6 +74,7 @@ class ViewController: UIViewController {
         }
         
     }
+    
     
     @IBAction func musicBtnPressed(_ sender: UIButton) {
         
@@ -86,27 +93,58 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            collection.reloadData()
+            view.endEditing(true)
+            
+        } else {
+            inSearchMode = true
+            
+            let lower = searchBar.text!.lowercased()
+            filteredPokemon = pokemon.filter({$0.name.range(of: lower) != nil})
+            collection.reloadData()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+}
 
 
 extension ViewController: UICollectionViewDelegate {
     
 }
 
+
 extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
-            let pokemonCharacter = pokemon[indexPath.row]
-            cell.configureCell(pokemonCharacter)
+            let pokemonCharacter: Pokemon!
             
+            if inSearchMode {
+                
+                pokemonCharacter = filteredPokemon[indexPath.row]
+                
+            } else {
+                pokemonCharacter = pokemon[indexPath.row]
+            }
+            
+            cell.configureCell(pokemonCharacter)
+
             return cell
         
         } else {
             
             return UICollectionViewCell()
         }
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -114,6 +152,11 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if inSearchMode {
+            return filteredPokemon.count
+        }
+        
         return pokemon.count
     }
     
